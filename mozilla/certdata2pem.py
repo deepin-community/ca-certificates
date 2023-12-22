a07e7e8477bf37c13eed54e045085342286af1e1
Tianyu Chen <sweetyfish@deepin.org>
2023-12-22 16:07:09 +0800

    feat: update ca-certificates to 20230311

--- mozilla/certdata2pem.py
+++ mozilla/certdata2pem.py
@@ -122,8 +122,8 @@ for obj in objects:
         if not obj['CKA_LABEL'] in trust or not trust[obj['CKA_LABEL']]:
             continue
 
-        cert = x509.load_der_x509_certificate(obj['CKA_VALUE'])
-        if cert.not_valid_after < datetime.datetime.now():
+        cert = x509.load_der_x509_certificate(bytes(obj['CKA_VALUE']))
+        if cert.not_valid_after < datetime.datetime.utcnow():
             print('!'*74)
             print('Trusted but expired certificate found: %s' % obj['CKA_LABEL'])
             print('!'*74)
